f6c06527942158f7aac7d4680db44196b9a64ab5
Wojciech Stancel <wojciech.stancel@gmail.com>
2023-09-03 13:39:24 +0200

    remove email info from settings

--- btre/settings.py
+++ btre/settings.py
@@ -151,15 +151,6 @@ MESSAGE_TAGS = {
 }
 
 
-# Email config
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'wojtek895@gmail.com'
-EMAIL_HOST_PASSWORD = 'eyzalvxommsswijt'
-EMAIL_USE_TLS = True
-
-
 try:
     from .local_settings import *
 except ImportError:
